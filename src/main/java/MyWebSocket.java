77744c1ff50ca19a94319bd2c81005b121be0050
mmlvkk <mmlvkk@gmail.com>
2016-05-18 13:48:46 +0800

    test

--- src/main/java/MyWebSocket.java
+++ src/main/java/MyWebSocket.java
@@ -8,6 +8,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
  
+
 //该注解用来指定一个URI，客户端可以通过这个URI来连接到WebSocket。类似Servlet的注解mapping。无需在web.xml中配置。
 @ServerEndpoint("/websocket")
 public class MyWebSocket {
